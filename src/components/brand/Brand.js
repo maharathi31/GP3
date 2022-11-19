9e3d6131173f9ffea1487a00609aa3d55eb40049
maharathi31 <hriday.rathi@plaksha.edu.in>
2022-11-19 16:08:22 +0530

    day-2

--- src/components/brand/Brand.js
+++ src/components/brand/Brand.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import "./Brand.css"
+import { google,slack,atlassian,dropbox,shopify } from './imports'
  const Brand=()=> {
   return (
-    <div>Brand</div>
+    <div className='gpt3__brand'>
+      <img src={google}></img>
+      <img src={slack}></img>
+      <img src={atlassian}></img>
+      <img src={dropbox}></img>
+      <img src={shopify}></img>
+    </div>
   )
 }
 export default Brand;
--- src/containers/whatGpt3/WhatGpt3.js
+++ src/containers/whatGpt3/WhatGpt3.js
@@ -3,7 +3,30 @@ import React from 'react'
 
 const WhatGpt3 = () => {
   return (
-    <div>WhatGpt3</div>
+    <div className="whatgpt3" id="whatgpt3">
+    <div className="part1">
+      <h2>What is GPT-3</h2>
+      <p>We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.</p>
+    </div>
+    <div className="part2 gradient__text">
+    <h2>The possibilities are beyond imagination</h2>
+    <p>Explore the library</p>
+    </div>
+    <div className="part3">
+      <div className="chatbots">
+      <h2>Chatbots</h2>
+    <p>We so opinion friends me message as delight. Whole front do of plate heard oh ought. </p>
+      </div>
+      <div className="knowledge">
+      <h2>Knowledge</h2>
+    <p>At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b </p>
+        </div>
+        <div className="education">
+        <h2>Education</h2>
+    <p>At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b</p>
+        </div>
+    </div>
+    </div>
   )
 }
 
