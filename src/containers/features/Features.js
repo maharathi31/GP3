c185ef923436769ca731d634b035038979ce9719
maharathi31 <hriday.rathi@plaksha.edu.in>
2022-11-20 20:20:51 +0530

    day-3

--- src/containers/features/Features.js
+++ src/containers/features/Features.js
@@ -2,6 +2,30 @@ import "./Features.css";
 import React from 'react'
 export default function Features() {
   return (
-    <div>Features</div>
+    <div className="features">
+      <div className="f1 gradient__text">
+      <h2>The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h2>
+      <p>Request Early Access to Get Started</p>
+      </div>
+      <div className="f2p">
+      <div className="f2">
+      <h3>Improving end distrusts instantly </h3>
+      <p>From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.</p>
+        </div>
+        <div className="f3">
+      <h3>Improving end distrusts instantly </h3>
+      <p>From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.</p>
+        </div>
+        <div className="f3">
+      <h3>Improving end distrusts instantly </h3>
+      <p>From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.</p>
+        </div>
+        <div className="f3">
+      <h3>Improving end distrusts instantly </h3>
+      <p>From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.</p>
+        </div>
+        </div> 
+
+    </div>
   )
 }
